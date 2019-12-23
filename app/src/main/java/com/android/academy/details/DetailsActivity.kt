7030a4729df64912cfaa2c23d890cef8b7027a35
TechDev40 <Techdev40@gmail.com>
2019-12-23 09:30:41 +0200

    Update Files and add pictures to res folder

--- app/src/main/java/com/android/academy/details/DetailsActivity.kt
+++ app/src/main/java/com/android/academy/details/DetailsActivity.kt
@@ -1,13 +1,23 @@
 package com.android.academy.details
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.android.academy.R
+import kotlinx.android.synthetic.main.activity_details.*
 
 class DetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+
+        details_btn_trailer.setOnClickListener {
+            val trailerUrl = getString(R.string.infinity_war_trailer)
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(trailerUrl))
+            startActivity(browserIntent)
+        }
     }
 }
+
