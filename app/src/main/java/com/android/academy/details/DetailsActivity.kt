9656840a7d45c8e785ee36c442c9db654db541dd
TechDev40 <Techdev40@gmail.com>
2019-12-23 08:28:11 +0200

    Update Files and add pictures to res folder

--- app/src/main/java/com/android/academy/details/DetailsActivity.kt
+++ app/src/main/java/com/android/academy/details/DetailsActivity.kt
@@ -1,12 +1,13 @@
-package com.android.academy
+package com.android.academy.details
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.academy.R
 
 class DetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_details)
     }
 }
